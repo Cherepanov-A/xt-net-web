291c930ea167eee1c4e77822f9215c45d1345e18
winter6644 <winter6644@live.com>
2020-02-16 21:44:52 +0400

    Without authorization

--- Epam.XT2019.Task6/Epam.XT2019.Task6.WebPL/Model/LogicProvider.cs
+++ Epam.XT2019.Task6/Epam.XT2019.Task6.WebPL/Model/LogicProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Epam.XT2019.Task6.Ioc;
 using Epam.XT2019.Task6.LogicContracts;
+using Epam.XT2019.Task6.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,18 @@ namespace Epam.XT2019.Task6.WebPL.Model
         public static bool CrtUsr(string id, string name, string dateOfBirth)
         {
             return uLogic.CreateUser(id, name, dateOfBirth);
+        }
+        public static bool DltUsr(string id)
+        {
+            return uLogic.DeleteUser(id);
+        }
+        public static List<User> DsplUsrs()
+        {
+            return uLogic.DisplayUsers();
+        }
+        public static bool CrtAwd(string id, string name)
+        { 
+            return awLogic.CreateAward(id, name);
         }
         //bool success = uLogic.CreateUser(id, name, dateOfBirth);
     }
