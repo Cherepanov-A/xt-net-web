a9caad543e6e51743e40c98a51685bc2daf50b10
winter6644 <winter6644@live.com>
2020-02-16 20:21:05 +0400

    doesn't login

---
+++ Epam.XT2019.Task6/Epam.XT2019.Task6.WebPL/Model/LogicProvider.cs
@@ -0,0 +1,20 @@
+﻿using Epam.XT2019.Task6.Ioc;
+using Epam.XT2019.Task6.LogicContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Epam.XT2019.Task6.WebPL.Model
+{
+    public class LogicProvider
+    {
+        static IUserLogic uLogic = DependencyResolver.ULogic;
+        static IAwardLogic awLogic = DependencyResolver.ALogic;
+        public static bool CrtUsr(string id, string name, string dateOfBirth)
+        {
+            return uLogic.CreateUser(id, name, dateOfBirth);
+        }
+        //bool success = uLogic.CreateUser(id, name, dateOfBirth);
+    }
+}
---
+++ Epam.XT2019.Task6/Epam.XT2019.Task6.WebPL/Model/WebPLRoleProvider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace Epam.XT2019.Task6.WebPL.Model
+{
+    public class WebPLRoleProvider : RoleProvider
+    {
+        public override string[] GetRolesForUser(string username)
+        {
+            if (username == "Andrey") return new string[] { "Admin", "User" };
+            else return new string[] { };            
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            return (username == "Andrey" && roleName == "Admin") ||
+                (username == "Andrey" && roleName == "User");
+        }
+
+        #region NOT_IMPLEMENTED
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        } 
+        #endregion
+    }
+}
